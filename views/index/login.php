dcf66653e8dce132e647233ff53279246640446e
duonghuupha <you@example.com>
2022-11-08 22:52:51 +0700

    asd

--- views/index/login.php
+++ views/index/login.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<meta charset="utf-8" />
-		<title>LongBien - Hight School</title>
+		<title>Trường THCS Long Biên :: Quận Long Biên</title>
 		<meta name="description" content="User login page" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 		<link rel="stylesheet" href="<?php echo URL.'/styles' ?>/css/roboto.css" />
