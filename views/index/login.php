d3cb88c0b70e68d8f5e80e36f9dc50fb3bce0296
duonghuupha <you@example.com>
2022-11-24 23:28:40 +0700

    vzxcv

--- views/index/login.php
+++ views/index/login.php
@@ -36,7 +36,7 @@
 								<h1>
 									<i class="ace-icon fa fa-leaf green"></i>
 									<span class="red">LB</span>
-									<span class="white" id="id-text2">Secondary School</span>
+									<span class="grey" id="id-text2">Secondary School</span>
 								</h1>
 								<h4 class="blue" id="id-company-text">&copy; THCS Long Biên</h4>
 							</div>
